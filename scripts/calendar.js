da9ca792e5411ddc508c854349b35dd01c67920d
Michael Ball <cycomachead@gmail.com>
2014-05-16 21:38:53 -0700

    add mirror of sp14 site

---
+++ scripts/calendar.js
@@ -0,0 +1,28 @@
+// this is an idea for making the site easier to update, maybe..
+labData = {
+  'intro1' : {
+      title: 'Welcome to Snap!',
+      link: 'intro/broadcast-animations-music.topic',
+      video: 'http://www.youtube.com/watch?v=Aub6BAxAT-c&amp;list=PLAE5AE3CD22628741&amp;index=1&amp;feature=plpp_video'
+  },
+  'lab1' : {
+      title: 'here',
+      link: 'here',
+      video: 'here1'
+  },
+}
+
+lectures = {
+  1 : {
+      title: 'Welcome to Snap!',
+      link: '',
+      video: 'http://www.youtube.com/watch?v=Aub6BAxAT-c&amp;list=PLAE5AE3CD22628741&amp;index=1&amp;feature=plpp_video',
+      rq: 0
+  },
+  2 : {
+      title: 'Build Your Own Blocks',
+      link: '',
+      video: '',
+      rq: 1
+  },
+}
