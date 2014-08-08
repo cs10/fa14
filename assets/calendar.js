25325daa290ff375188a3c1c415f99be6acef3b0
Michael Ball <cycomachead@gmail.com>
2014-08-08 02:30:33 -0700

    WIP on the stupid calendar refactor.
    Moved some other files around to be more clear
    Added bootstrap and jQuery for offline testing

--- assets/calendar.js
+++ assets/calendar.js
@@ -1,19 +1,17 @@
 // this is an idea for making the site easier to update, maybe..
 // This is major WIP and the API / format for this has yet to be finalized
-lectures = [
-    {
+lecture = {
         title: '',
-        slides: 'url',
+        url: 'url',
         video: 'url',
-        style: 'optional classes to apply'
-    }
-];
+        classes: 'optional classes to apply'
+};
 
 /* DATA MODELS -- OPTIONAL DATA NOTED */
 reading = {
     title: 'Name',
     url: 'duh',
-    type: 'required hard' // or whatver
+    classes: 'required hard' // or whatver
 };
 
 lab = {
@@ -25,22 +23,22 @@ lab = {
 
 discussion = {
     title: '',
-    files: false // OPTIONAL -- default false
+    url: '' // OPTIONAL -- default is none.
 };
 
 cs10 = {};
+window.cs10 = cs10;
 
 cs10.newLabObject = function(title, url, rq, video) {
     // FIXME -- better handle the URL via config
     // TODO  -- How does "Project Work" or no lab fit into this?
     baseURL = '../labs/llab/html/topic.html?topic=';
-    var labObj = {
-        'title': title,
-    };
-    if (title === null) {
-        // set title
+    var labObj = { type: 'Lab' };
+    if (!title) {
+        labObj.title = 'No Lab'
         return labObj;
     }
+    labObj.title = title;
     if (url) {
         labObj.url = baseURL + url;
     }
@@ -53,12 +51,106 @@ cs10.newLabObject = function(title, url, rq, video) {
     return labObj;
 };
 
-var lab = cs10.newLabObject,
-    readings = function() {},
-    lect = function() {},
-    disc = function() {},
-    hw = function() {};
+cs10.newReadingsObject = function(item) {
+    return {
+        type: 'Readings',
+        title: 'These are a pain in the ass'
+    }
+};
 
+cs10.newLectureObject = function(title, guest) {
+    var obj = { type: 'Lecture' };
+
+    if (!title) {
+        obj.title = 'No Lecture';
+        return obj;
+    }
+    // Global Counter for lecture
+    cs10.lectureCounter = cs10.lectureCounter || 0;
+    cs10.lectureCounter += 1;
+    var count = cs10.lectureCounter;
+    var lectureURL = 'lectures/' + (count < 10 ? '0' : '' ) + count + '/';
+    obj.title = title;
+    obj.url = lectureURL
+    if (guest) {
+        obj.guest = guest;
+    }
+    return obj;
+};
+
+cs10.newDiscussionObject = function(title, files) {
+    var obj = { 'type' : 'Discussion' };
+    if (!title) {
+        obj.title = 'No Discussion';
+        return obj;
+    }
+    // Global Counter for lecture
+    cs10.discussionCounter = cs10.discussionCounter || 0;
+    cs10.discussionCounter += 1;
+    obj.title = title;
+
+    if (files) {
+        var count = cs10.discussionCounter;
+        var discussionURL = 'discussion/' + (count < 10 ? '0' : '' ) + count + '/';
+        obj.url = dsicussionURL;
+    }
+
+    return obj;
+};
+cs10.newHomeworkObject = function(title, url) {
+    var obj = { type: 'Homework' };
+
+    if (!title) {
+        obj.title = 'No Homework!<br />But you might want to check next week\'s';
+        return obj;
+    }
+
+    obj.title = title;
+    obj.classes = 'assignmentDue';
+    if (url) {
+        obj.url = url;
+    }
+    return obj;
+};
+
+var lab      = cs10.newLabObject,
+    readings = cs10.newReadingsObject,
+    lect     = cs10.newLectureObject,
+    disc     = cs10.newDiscussionObject,
+    hw       = cs10.newHomeworkObject;
+
+cs10.renderObject = function(obj) {
+    obj.classes = obj.classes || ' ';
+    var html = $(document.createElement('div')).attr(
+        { 'class': obj.classes }
+    );
+
+    var heading = $(document.createElement('h3')).html(obj.type);
+    html.append(heading);
+    var content;
+    if (obj.url) {
+        content = $(document.createElement('a')).attr(
+            { 'href': obj.url}).html(obj.title);
+    } else {
+        content = $(document.createElement('span')).html(obj.title);
+    }
+    if (obj.video) {
+        content.append('<br />');
+        var video = $(document.createElement('a')).attr(
+            { 'href': obj.video,
+              'target': '_blank'
+            }).html('Watch a video here.');
+        content.append(video);
+    }
+    if (obj.RQ) {
+        content.append('<br />');
+        content.append($(document.createElement('b')).html('Reading Quiz ' +
+        obj.RQ));
+    }
+
+    html.append(content);
+    return html.html();
+}
 
 // ==================================================
 // ==========     SCHEDULE ITEMS           ==========
@@ -234,18 +326,39 @@ cs10.week17 = [
     disc('Getting Started With <span class=“snap”>snap</span>'),
     hw('Start on HW1') ];
 
-cs10.schedule = [ ];
+cs10.schedule = [];
 cs10.buildCal = function() {
     var calDiv     = $('#main-cal'),
         calPills   = $('#cal-sidebar'),
-        calContent = $('#calContent'),
-        selector, title;
-    for(var i = 1; i <= 17; i += 1) {
+        calContent = $('#cal-content'),
+        pillsList  = '',
+        calData    = '',
+        selector, title, i = 1;
+
+    for (; i <= 17; i += 1) {
         cs10.schedule.push(cs10['week' + i]);
         selector = 'cal-week-' + i;
         title = 'Week ' + i;
-        calPills.append('<li><a href="' + selector + '" role="tab" data-toggle="tab">' + title + '</a></li>');
+        // TODO: Add the date.
+        pillsList += '<li><a href="' + selector + '" id="' + selector +
+        '" target="' + selector + '" role="tab" data-toggle="tab">' + title + '</a></li>\n';
     }
+
+    /* Bootstrap div format:
+     * <div class="tab-pane active" id="home"></div>
+     */
+    var week, weekStr, isActive;
+    for (i = 0; i < 17; i += 1) {
+        // TODO this should be the current week of the school year.
+        isActive = i == 0 ? 'active' : '';
+        selector = 'cal-week-' + (i + 1);
+        week = cs10.schedule[i].map(cs10.renderObject);
+        weekStr = week.join('\n');
+        calData += '<div class="tab-pane ' + isActive + '" id="' + selector +
+                 '">' + weekStr + '</div>\n'
+    }
+    calPills.html(pillsList);
+    calContent.html(calData);
 };
 
-$(document).ready(cs10.buildCal);
+        // $(document).ready(cs10.buildCal);
