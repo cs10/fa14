061218076a09748959acfe624f40c2ba8c51543f
Michael Ball <cycomachead@gmail.com>
2014-11-18 02:37:03 -0800

    test gcal fixes

--- assets/lib/gcal.js
+++ assets/lib/gcal.js
@@ -39,7 +39,7 @@ var applyAll = fc.applyAll;
 fc.sourceNormalizers.push(function(sourceOptions) {
     if (sourceOptions.dataType == 'gcal' ||
         sourceOptions.dataType === undefined &&
-        (sourceOptions.url || '').match(/^(http|https):\/\/www.google.com\/calendar\/feeds\//)) {
+        (sourceOptions.url || '').match(/^(http|https):\/\/www.googleapis.com\/calendar\/v3\/calendars//)) {
             sourceOptions.dataType = 'gcal';
             if (sourceOptions.editable === undefined) {
                 sourceOptions.editable = false;
@@ -71,15 +71,15 @@ function transformOptions(sourceOptions, start, end) {
     }
 
     return $.extend({}, sourceOptions, {
-        url: sourceOptions.url.replace(/\/basic$/, '/full') + '?alt=json-in-script&callback=?',
+        url: sourceOptions.url + '&callback=?',
         dataType: 'jsonp',
         data: data,
         startParam: false,
         endParam: false,
         success: function(data) {
             var events = [];
-            if (data.feed.entry) {
-                $.each(data.feed.entry, function(i, entry) {
+            if (data.items) {
+                $.each(data.items, function(i, entry) {
                     var startStr = entry['gd$when'][0]['startTime'];
                     var start = parseISO8601(startStr, true);
                     var end = parseISO8601(entry['gd$when'][0]['endTime'], true);
@@ -97,14 +97,21 @@ function transformOptions(sourceOptions, start, end) {
                         addDays(end, -1); // make inclusive
                     }
                     events.push({
-                        id: entry['gCal$uid']['value'],
-                        title: entry['title']['$t'],
-                        url: getRoomUrl(entry['gd$where'][0]['valueString']),
-                        start: start,
-                        end: end,
-                        allDay: allDay,
-                        location: entry['gd$where'][0]['valueString'],
-                        description: entry['content']['$t']
+                        id: entry.id,
+                        title: entry.summary,
+                        start: entry.start.dateTime || entry.start.date,
+                        end: entry.end.dateTime || entry.end.date,
+                        url: getRoomUrl(entry.htmlLink), // My mod
+                        location: entry.location,
+                        description: entry.description
+                        // id: entry['gCal$uid']['value'],
+                        // title: entry['title']['$t'],
+                        // url: getRoomUrl(entry['gd$where'][0]['valueString']),
+                        // start: start,
+                        // end: end,
+                        // allDay: allDay,
+                        // location: entry['gd$where'][0]['valueString'],
+                        // description: entry['content']['$t']
                     });
                 });
             }
@@ -127,3 +134,87 @@ fc.gcalFeed = function(url, sourceOptions) {
 
 
 })(jQuery);
+//
+// (function(factory) {
+//     if (typeof define === 'function' && define.amd) {
+//         define([ 'jquery' ], factory);
+//     }
+//     else {
+//         factory(jQuery);
+//     }
+// })(function($) {
+//
+//
+// var fc = $.fullCalendar;
+// var applyAll = fc.applyAll;
+//
+//
+// fc.sourceNormalizers.push(function(sourceOptions) {
+//
+//     if (sourceOptions.dataType == 'gcal' ||
+//         sourceOptions.dataType === undefined &&
+//         (sourceOptions.url || '').match(/^(http|https):\/\/www.googleapis.com\/calendar\/v3\/calendars/)) {
+//             sourceOptions.dataType = 'gcal';
+//             if (sourceOptions.editable === undefined) {
+//                 sourceOptions.editable = false;
+//             }
+//         }
+// });
+//
+//
+// fc.sourceFetchers.push(function(sourceOptions, start, end, timezone) {
+//     if (sourceOptions.dataType == 'gcal') {
+//         return transformOptions(sourceOptions, start, end, timezone);
+//     }
+// });
+//
+//
+// function transformOptions(sourceOptions, start, end, timezone) {
+//
+//     var success = sourceOptions.success;
+//     var data = $.extend({}, sourceOptions.data || {}, {
+//         'singleEvents' : true,
+//         'maxResults': 250,
+//         'timeMin': start.format('YYYY-MM-DD[T]HH:mm:ssZ'),
+//         'timeMax': end.format('YYYY-MM-DD[T]HH:mm:ssZ'),
+//     });
+//
+//     return $.extend({}, sourceOptions, {
+//         url: sourceOptions.url + '&callback=?',
+//         dataType: 'jsonp',
+//         data: data,
+//         success: function(data) {
+//             var events = [];
+//             if (data.items) {
+//                 $.each(data.items, function(i, entry) {
+//                     events.push({
+//                         id: entry.id,
+//                         title: entry.summary,
+//                         start:  entry.start.dateTime || entry.start.date,
+//                         end: entry.end.dateTime || entry.end.date,
+//                         url: entry.htmlLink,
+//                         location: entry.location,
+//                         description: entry.description || '',
+//                     });
+//
+//                 });
+//             }
+//             var args = [events].concat(Array.prototype.slice.call(arguments, 1));
+//             var res = applyAll(success, this, args);
+//             if ($.isArray(res)) {
+//                 return res;
+//             }
+//             return events;
+//         }
+//     });
+//
+// }
+//
+//
+// // legacy
+// fc.gcalFeed = function(url, sourceOptions) {
+//     return $.extend({}, sourceOptions, { url: url, dataType: 'gcal' });
+// };
+//
+//
+// });
